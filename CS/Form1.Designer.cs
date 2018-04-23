6717060d80e92ded824ad755258a499f2fbb339f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SchedulerCustomTooltip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.schedulerControl1.Location = new System.Drawing.Point(16, 15);
+            this.schedulerControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(819, 417);
+            this.schedulerControl1.Start = new System.DateTime(2008, 9, 4, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.ToolTipController = this.toolTipController1;
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            // 
+            // toolTipController1
+            // 
+            this.toolTipController1.BeforeShow += new DevExpress.Utils.ToolTipControllerBeforeShowEventHandler(this.toolTipController1_BeforeShow);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 484);
+            this.Controls.Add(this.schedulerControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.Utils.ToolTipController toolTipController1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to display custom tooltips for appointments, resource headers and day headers
+
+
+<p>This example illustrates how to display custom tooltips for appointments, resource headers and day headers. We assign a ToolTipController Class instance to the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraSchedulerSchedulerControl_ToolTipControllertopic"><u>SchedulerControl.ToolTipController Property</u></a> and handle the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressUtilsToolTipController_BeforeShowtopic"><u>ToolTipController.BeforeShow Event</u></a>. In this event handler, we examine the type of the object in the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressUtilsToolTipController_ActiveObjecttopic"><u>ToolTipController.ActiveObject Property</u></a> and create a custom tooltip on the fly.</p><p><strong>See Also:</strong></p><p><a href="https://www.devexpress.com/Support/Center/p/E155">How to customize the tooltips shown for appointments</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E414">How to show a tooltip for time cells in a scheduler view</a></p>
+
+<br/>
+
+
